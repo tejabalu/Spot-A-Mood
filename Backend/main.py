64e58f2d8762eb2a6e12037d77d60bc5b458886d
teja balu <tejabalu15@gmail.com>
2022-04-18 03:29:59 -0700

    second level auth

---
+++ Backend/main.py
@@ -0,0 +1,56 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import requests
+import constants
+
+cid = constants.cid
+secret = constants.secret
+
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+artist_name = []
+track_name = []
+popularity = []
+track_id = []
+for i in range(0,100,50):
+    track_results = sp.search(q='year:2020', type='track', limit=50,offset=i)
+    for i, t in enumerate(track_results['tracks']['items']):
+        artist_name.append(t['artists'][0]['name'])
+        track_name.append(t['name'])
+        track_id.append(t['id'])
+        popularity.append(t['popularity'])
+
+print(artist_name)
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+# POST
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': cid,
+    'client_secret': secret,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+print(access_token)
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+# base URL of all Spotify API endpoints
+BASE_URL = 'https://api.spotify.com/v1/'
+
+# Track ID from the URI
+track_id = '6y0igZArWVi6Iz0rj35c1Y'
+
+# actual GET request with proper header
+r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+
+r = r.json()
+print(r)
