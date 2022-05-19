c7006c937a8a21cfbc2c5e8fea29e706d8b8b239
tejabalu <tejabalu15@gmail.com>
2022-05-19 11:03:52 -0700

    Sample Data generation

--- Backend/spotifyHistory.py
+++ Backend/spotifyHistory.py
@@ -1,21 +1,20 @@
 import spotipy
 import spotipy.util as util
 import types
-import constants
 
 # _________________________________________________
 # initialize mongodb
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
-import certifi
+# from pymongo import MongoClient
+# from pymongo.server_api import ServerApi
+# import certifi
 
-client = MongoClient(
-    "mongodb+srv://tejabalu:tejabalu@cluster0.p2d72.mongodb.net/test?retryWrites=true&w=majority",
-    tlsCAFile=certifi.where(),
-)
+# client = MongoClient(
+#     "mongodb+srv://tejabalu:tejabalu@cluster0.p2d72.mongodb.net/test?retryWrites=true&w=majority",
+#     tlsCAFile=certifi.where(),
+# )
 
-db = client.test1
-dbhistory = db.dbhistory
+# db = client.test1
+# dbhistory = db.dbhistory
 
 # _________________________________________________
 # spotify
@@ -26,8 +25,8 @@ def current_user_recently_played(self, limit=50):
 token = util.prompt_for_user_token(
     # username="xxxxxxxxxxxxxx",
     scope="user-read-recently-played user-read-private user-top-read user-read-currently-playing",
-    client_id=constants.cid,
-    client_secret=constants.secret,
+    client_id="",
+    client_secret="",
     redirect_uri="http://localhost:7777/callback",
 )
 
