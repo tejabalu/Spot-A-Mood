88a3134945224b955d7b4bef7defabb15ffa71d0
chuan <cchang30@seattlecentral.edu>
2022-06-03 00:08:14 -0700

    spotifyplot-needjsonfile

--- spotamoodfrontend/pages/SpotifyPlot.js
+++ spotamoodfrontend/pages/SpotifyPlot.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import Plot from "react-plotly.js";
 
 class SpotifyPlot extends Component {
@@ -6,6 +6,35 @@ class SpotifyPlot extends Component {
   constructor(props) {
     super(props);
     this.state = { data: [] };
+  }
+
+  // Call API upon component mount
+  componentDidMount() {
+    const endpoint = "JSON FILE GOES HERE";
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data });
+      });
+  }
+
+  // Change data structure
+  transformData(data) {
+    let plot_data = [];
+
+    let x = [];
+    let y = [];
+    data.map((each) => {
+      x.push(each.date_of_interest);
+      y.push(each.case_count);
+    });
+    plot_data["x"] = x;
+    plot_data["y"] = y;
+
+    console.log(plot_data);
+
+    return plot_data;
   }
 
   render() {
@@ -16,14 +45,16 @@ class SpotifyPlot extends Component {
             {
               type: "radar",
               mode: "lines",
-              //   x: this.transformData(this.state.data)["x"],
-              //   y: this.transformData(this.state.data)["y"],
+              x: this.transformData(this.state.data)["x"],
+              y: this.transformData(this.state.data)["y"],
               marker: { color: "#ed022d" },
             },
           ]}
-          layout={{ width: 1000, height: 500, title: "" }}
+          layout={{ width: 1000, height: 500, title: "Covid Case Count" }}
         />
       </div>
     );
   }
 }
+
+export default SpotifyPlot;
--- spotamoodfrontend/pages/_app.js
+++ spotamoodfrontend/pages/_app.js
@@ -1,13 +1,13 @@
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 import Landing from "../pages/Landing";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { createContext, useContext } from "react";
 
 import { SpotifyContent } from "../content/SpotifyContent";
 
-
 import dynamic from "next/dynamic";
+import SpotifyPlot from "./SpotifyPlot";
 // const ApiCharts = dynamic(() => import("./ApiCharts"), {
 // 	ssr: false,
 // });
@@ -16,15 +16,16 @@ import dynamic from "next/dynamic";
 // });
 
 function MyApp({ Component, pageProps }) {
-	// console.log("MyApp");
-    const  isAuthenticated   = useContext(SpotifyContent)
+  // console.log("MyApp");
+  const isAuthenticated = useContext(SpotifyContent);
 
-	return (
-		<Layout>
-			<Component {...pageProps} />
-            <Landing />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+      <Landing />
+      {/* <SpotifyPlot /> */}
+    </Layout>
+  );
 }
 
 export default MyApp;
